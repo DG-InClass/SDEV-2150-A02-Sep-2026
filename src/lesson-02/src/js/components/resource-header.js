4e82ce181a72eb310503ad73189793e121e66c00
Dan Gilleland <dgilleland@nait.ca>
2026-09-10 10:50:26 -0600

    Bootstrap my custom elements

---
+++ src/lesson-02/src/js/components/resource-header.js
@@ -0,0 +1,24 @@
+// resource-header.js
+const template = document.createElement('template');
+template.innerHTML = `<div>Replace Me</div>`;
+
+class ComponentName extends HTMLElement {
+    constructor() {
+        // The job of the constructor is to ensure
+        // that all the properties have meaningful
+        // values.
+        super(); // call the constructor of the class we are inheriting
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback() {
+        // our opportunity to do whatever "initialization"
+        this.render();
+    }
+
+    render() {
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('component-name', ComponentName);
