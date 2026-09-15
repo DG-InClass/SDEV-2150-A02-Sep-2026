54ce1f992df4ea84204f0715581b7f1e7b601574
Dan Gilleland <dgilleland@nait.ca>
2026-09-15 10:22:18 -0600

    pnpm install + main.js

--- src/lesson-03/src/js/main.js
+++ src/lesson-03/src/js/main.js
@@ -53,5 +53,14 @@ const resultData = [
 ];
 
 // TODO: Pass data to resource-results component
+const resultsComponent = document.querySelector('resource-results');
+resultsComponent.results = resultData;
 
 // TODO: Listen for resourceSelected event and update resource-details component
+const detailsComponent = document.querySelector('resource-details');
+resultsComponent.addEventListener('resource-selected', (event) => {
+  const { resource } = event.detail; // Object Destructuring
+  //      \______/             |
+  //   prop name             detail.resource
+  detailsComponent.resource = resource;
+});
